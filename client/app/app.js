0c42988eb190265ac588e3c70f2d9359754eaa8d
tanching <tanching@outlook.com>
2016-06-13 15:52:07 +0800

    add contacts file to my branch

---
+++ client/app/app.js
@@ -0,0 +1,38 @@
+var app = angular.module('app', ['ngTouch', 'ui.grid', 'ui.router'] );
+
+	app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+	    $urlRouterProvider.otherwise('/');
+ 	    $stateProvider
+	      .state('home', {
+ 	        url: '/',
+	        views: {
+                viewA: {template: "Welcome to BulletLead"},
+				viewB: {template: ""}
+            }
+ 	      })
+		   .state('leadlist', {
+ 	        url: '/leadlist',
+		
+	        views: {
+				viewA: {templateUrl: 'ContactsMgmt/partials/contactsTabs.html'},
+				viewB: {templateUrl: 'ContactsMgmt/partials/contactsLeadList.html', controller: 'contactsMainController'}
+            }
+ 	      })
+		   .state('blacklist', {
+ 	        url: '/blacklist',
+		
+	        views: {
+				viewA: {templateUrl: 'ContactsMgmt/partials/contactsTabs.html'},
+				viewB: {templateUrl: 'ContactsMgmt/partials/contactsBlackList.html', controller: 'contactsMainController'}
+            }
+ 	      })
+
+           .state('business', {
+               url: '/business',
+
+               views: {
+                   viewA: {templateUrl: 'ScrapingMgmt/partials/scrapTabs.html'},
+                   viewB: {templateUrl: 'ScrapingMgmt/partials/beforeScrap.html'}
+               }
+           })
+ 	}]);
