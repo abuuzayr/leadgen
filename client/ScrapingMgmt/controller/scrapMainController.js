3657f8902e1d3c1c42f63474ad3d094c00cf6fd5
tanching <tanching@outlook.com>
2016-06-13 12:23:27 +0800

    create 'controller' and 'partial' folders

---
+++ client/ScrapingMgmt/controller/scrapMainController.js
@@ -0,0 +1,22 @@
+app.controller('scrapMainController',['$scope', function($scope){
+    $scope.myData = [
+        {
+            name: 'name1',
+            email: 'testemail1',
+            company: 'test company',
+            number: '123'
+        },
+        {
+            name: 'name2',
+            email: 'testemail2',
+            company: 'test company',
+            number: '456'
+        },
+        {
+            name: 'name3',
+            email: 'testemail3',
+            company: 'test company',
+            number: '789'
+        }
+    ];
+}]);
