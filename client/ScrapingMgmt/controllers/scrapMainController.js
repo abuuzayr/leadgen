7f11e0f7b22c623baa27e5726e32729fc90e0596
tanching <tanching@outlook.com>
2016-06-14 14:39:49 +0800

    change  to vm for scrap controller

--- client/ScrapingMgmt/controllers/scrapMainController.js
+++ client/ScrapingMgmt/controllers/scrapMainController.js
@@ -1,6 +1,7 @@
-app.controller('scrapMainController',['$scope', function($scope){
-    
-    $scope.myData = [
+app.controller('scrapMainController', function(){
+    var vm = this;
+
+    vm.gridOptions.data = [
         {
             'name': 'Lord Of the Rings',
             'email': 'lotr@email.com',
@@ -20,4 +21,37 @@ app.controller('scrapMainController',['$scope', function($scope){
             number: '789'
         }
     ];
-}]);
+
+    //store category and country
+    vm.fields = {};
+
+    vm.getData = function() {
+
+        vm.fields.push({
+            category: vm.scrapData.category,
+            country: vm.scrapData.country
+        });
+
+        //clear after fields has been added
+        vm.scrapData = {};
+    }
+
+    vm.gridOptions = {
+    enableSorting: true,
+    enableFiltering: true,
+    columnDefs: [
+      { field: 'firstName', headerCellClass: vm.highlightFilteredHeader },
+      { field: 'lastName', headerCellClass: vm.highlightFilteredHeader  },
+      { field: 'company', enableSorting: false },
+      { field: 'employed', filter: {
+        term: true,
+        type: uiGridConstants.filter.SELECT,
+        selectOptions: [ { value: true, label: 'true' }, { value: false, label: 'false' } ]
+        },
+        cellFilter: 'mapEmployed', headerCellClass: vm.highlightFilteredHeader },
+    ],
+    onRegisterApi: function( gridApi ) {
+      vm.gridApi = gridApi;
+    }
+  };  
+});
