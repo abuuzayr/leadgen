d363aa66994c8c16d7eb79bc3372bfdae337d568
tanching <tanching@outlook.com>
2016-06-16 09:46:36 +0800

    leadsResult only get from scrapLeads

--- client/ScrapingMgmt/services/leadsResult.js
+++ client/ScrapingMgmt/services/leadsResult.js
@@ -7,6 +7,30 @@ app.factory('leadsResult', ['$http', function($http) {
         return err; 
     });
 
+    //test 2 json files
+    /*var allLeads = {};
+    allLeads.getYPLeads = function() {
+        return $http.get('ScrapingMgmt/testFiles/scrapLeads.json') 
+            .success(function(data) { 
+                return data; 
+            }) 
+            .error(function(err) { 
+                return err; 
+            });
+    }
+
+    allLeads.getGoogleLeads = function() {
+        return $http.get('ScrapingMgmt/testFiles/scrapGoogle.json') 
+            .success(function(data) { 
+                return data; 
+            }) 
+            .error(function(err) { 
+                return err; 
+            });
+    }
+
+    return allLeads;*/
+
     // create a new object
     /*var leadsFactory = {};
 
