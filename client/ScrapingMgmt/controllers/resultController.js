dfec60ccbd76382e1d81028ff3b6634f5eccb94f
tanching <tanching@outlook.com>
2016-06-20 14:24:03 +0800

    add delete row and save to contacts function

--- client/ScrapingMgmt/controllers/resultController.js
+++ client/ScrapingMgmt/controllers/resultController.js
@@ -11,12 +11,42 @@ app.controller('resultController', ['$scope', 'shareData', '$http', 'uiGridConst
       return '';
     }
   };
+
+  rc.showResult = false;
+  
+  rc.showFunction = function() {
+    rc.showResult = true;
+    $timeout(function () { 
+      rc.showResult = false; 
+    }, 2000);
+  };
+
+  rc.deleteRow = function(row) {
+    var index = rc.gridOptions.data.indexOf(row.entity);
+    rc.gridOptions.data.splice(index, 1);
+  }
+
+  rc.responseMessage = "";
+  rc.symbol = true;
+
+  rc.saveToContacts = function() {
+    var myJsonString = JSON.stringify(rc.displayResult);
+    var response = $http.post('/api/Corporate/scrap',myJsonString);
+    response.success(function(data) {
+      rc.responseMessage = "Saved to Contacts!";
+    });
+    response.error(function(data) {
+      rc.responseMessage = "Error Occured";
+      rc.symbol = false;
+    })
+  }
   
   rc.gridOptions = {
     enableSorting: true,
     enableFiltering: true,
     data: 'rc.displayResult',
     columnDefs: [
+      { field: 'select', enableSorting:false, enableFiltering:false, cellTemplate: '<input type="checkbox" name="selectCheckBox" ng-click="rc.deleteRow(row)">' },
       { field: 'name', headerCellClass: rc.highlightFilteredHeader },
       { field: 'email', headerCellClass: rc.highlightFilteredHeader  },
       { field: 'company', enableSorting: false },
@@ -26,6 +56,4 @@ app.controller('resultController', ['$scope', 'shareData', '$http', 'uiGridConst
       rc.gridApi = gridApi;
     }
   };
-
-  
 }]);
