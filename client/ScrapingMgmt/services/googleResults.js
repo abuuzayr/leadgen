c246d746b335fe8b489dd1dea6cc9005de897d63
tanching <tanching@outlook.com>
2016-06-20 14:28:01 +0800

    add get request with parameter

--- client/ScrapingMgmt/services/googleResults.js
+++ client/ScrapingMgmt/services/googleResults.js
@@ -8,13 +8,23 @@ app.factory('googleResults', ['$http', function($http) {
         });
 
 
-    // create a new object
-    /*var leadsFactory = {};
+    // when there is valid api, use this
+    // if this cannot pass the data, put this in google controller
+    // return $http.get('ScrapingMgmt/testFiles/scrapGoogle.json',{params:{"category": gc.input.category, "country": gc.input.country}})
+    //     .success(function(data) {
+    //         return data;
+    //     })
+    //     .error(function(err) {
+    //         return err;
+    //     });
 
-    //get leads data from yellow page
-    leadsFactory.get = function(category,country) {
-        return $http.get('/api/leads/' + category,country);
-    };
-
-    return leadsFactory;*/
+    // var getGoogleLeads = function(category,country) {
+    //     return $http.get('ScrapingMgmt/testFiles/scrapGoogle.json')
+    //         .success(function(data) {
+    //             return data;
+    //         })
+    //         .error(function(err) {
+    //             return err;
+    //         });
+    // }
 }]);
