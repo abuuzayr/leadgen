4188a0260d1e6bd0c8f9a6705c4547d7422a0b9a
tanching <tanching@outlook.com>
2016-06-22 10:20:02 +0800

    add params to yp http.get

--- client/ScrapingMgmt/services/ypResults.js
+++ client/ScrapingMgmt/services/ypResults.js
@@ -1,20 +1,23 @@
 app.factory('ypResults', ['$http', function($http) { 
-    return $http.get('ScrapingMgmt/testFiles/scrapYP.json')
-        .success(function(data) {
-            return data;
-        })
-        .error(function(err) {
-            return err;
+    var scrapeYellowPageLeads = function() {
+        return $http({
+            method: 'GET',
+            url: 'ScrapingMgmt/testFiles/scrapYP.json'
         });
+    }
 
+    return {
+        scrapeYellowPageLeads: scrapeYellowPageLeads
+    }
 
-    // create a new object
-    /*var leadsFactory = {};
+    // when there is valid api, use this
+    // // var scrapeYellowPageLeads = function(category,country) {
+    //     return $http({
+    //         method: 'GET',
+    //         url: 'ScrapingMgmt/testFiles/scrapGoogle.json',
+    //         params: {country: country, category: category}
+    //     });
+    // }
 
-    //get leads data from yellow page
-    leadsFactory.get = function(category,country) {
-        return $http.get('/api/leads/' + category,country);
-    };
-
-    return leadsFactory;*/
+    
 }]);
