fb2aedb92e267fce0627dda7c9b9494fb2c61231
tanching <tanching@outlook.com>
2016-06-22 16:40:48 +0800

    include controller for userMgmt profile

---
+++ client/UserMgmt/controllers/userMgmtProfileController.js
@@ -0,0 +1,6 @@
+app.controller('userMgmtController', ['$scope', '$http', 'uiGridConstants', '$q', '$location', '$timeout', function ($scope, $http, uiGridConstants, $q, $location, $timeout) {
+    var up = this;
+
+    up.moreDetails = false;
+    
+}]);
