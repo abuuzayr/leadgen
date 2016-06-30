2f213d7aee0b25758835456579d8896a9bee83f0
tanching <tanching@outlook.com>
2016-06-30 10:53:17 +0800

    create consumer folder for scrap service

---
+++ client/ScrapingMgmt/services/consumer/consumerShareData.js
@@ -0,0 +1,23 @@
+app.factory('consumerShareData',function() {
+    var savedData = [];
+    
+    var setData = function(data) {
+        savedData = data;
+    }
+
+    var getData = function() {
+        //console.log(saveData);
+        return savedData;
+    }
+
+    var addLead = function(newLead) {
+        savedData.push(newLead);
+        console.log('new lead is ' + newLead);
+    }
+
+    return {
+        setData: setData,
+        getData: getData,
+        addLead: addLead
+    }
+});
