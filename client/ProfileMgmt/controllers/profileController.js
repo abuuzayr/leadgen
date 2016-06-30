ceb3011887d560618d89f48851c0c67d366b293d
tanching <tanching@outlook.com>
2016-06-30 10:42:23 +0800

    get details for profile page

--- client/ProfileMgmt/controllers/profileController.js
+++ client/ProfileMgmt/controllers/profileController.js
@@ -1,7 +1,13 @@
-app.controller('profileController', ['$scope', '$http', '$q', '$location', '$timeout', function ($scope, $http, $q, $location, $timeout) {
+app.controller('profileController', ['$scope', '$http', '$q', '$location', '$timeout', 'getDetails', function ($scope, $http, $q, $location, $timeout, getDetails) {
+    
     var pc = this;
     //properties: email,new password, retypePassword
-    pc.user = {};
+
+    getDetails.success(function(data) {
+        pc.userName = data[0].name;
+        pc.userEmail = data[0].email;
+        pc.userPassword = data[0].password;
+    });
 
     //post to database the updated details
     pc.submitDetails = function() {
