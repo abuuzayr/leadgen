c4c5c36f0a444a93a0bd9618ceeb74a79b48c1f7
tanching <tanching@outlook.com>
2016-06-30 10:54:07 +0800

    create company profile services

---
+++ client/UserMgmt/services/companyProfile.js
@@ -0,0 +1,9 @@
+app.factory('companyProfile', ['$http', function($http) { 
+  return $http.get('UserMgmt/testFiles/companyProfileData.json')
+    .success(function(data) { 
+        return data; 
+    }) 
+    .error(function(err) { 
+        return err; 
+    }); 
+}]);
