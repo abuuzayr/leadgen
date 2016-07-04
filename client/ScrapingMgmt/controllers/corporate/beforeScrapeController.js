6f30c4be4c324fb4b9863888cd0d8c0dcc9d1921
tanching <tanching@outlook.com>
2016-07-04 17:50:34 +0800

    create corporate folder for controller

---
+++ client/ScrapingMgmt/controllers/corporate/beforeScrapeController.js
@@ -0,0 +1,19 @@
+app.controller('beforeScrapeController', ['$scope', '$http', 'uiGridConstants', '$q', '$location', '$timeout', 'shareInput', 'sendCountry', 
+                function ($scope, $http, uiGridConstants, $q, $location, $timeout, shareInput, sendCountry) {
+    var beforeCorporate = this;
+
+    beforeCorporate.category = "";
+    beforeCorporate.country = "Singapore"; 
+
+    beforeCorporate.listOfCountry;
+    //get country data
+    sendCountry.success(function(data) {
+         beforeCorporate.listOfCountry = data;
+    });
+
+    beforeCorporate.setInput = function() {
+        shareInput.setCategory(beforeCorporate.category);
+        shareInput.setCountry(beforeCorporate.country);
+    }
+    
+}]);
