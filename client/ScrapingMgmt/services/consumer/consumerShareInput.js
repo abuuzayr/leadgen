7f184069abb9e1a431ac77999b9602f6e19362a5
tanching <tanching@outlook.com>
2016-07-04 17:50:56 +0800

    create new share input file

---
+++ client/ScrapingMgmt/services/consumer/consumerShareInput.js
@@ -0,0 +1,16 @@
+app.factory('consumerShareInput',function() {
+    var categoryInput = "";
+
+    var getCategory = function () {
+        return categoryInput;
+    }
+
+    var setCategory = function(userCategory) {
+        categoryInput = userCategory;
+    }
+
+    return {
+        getCategory: getCategory,
+        setCategory: setCategory
+    }
+});
