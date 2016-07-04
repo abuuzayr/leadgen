b7f5446542ad76289be2c1bb706ef4904199131e
tanching <tanching@outlook.com>
2016-07-04 14:05:45 +0800

    include url for post method

--- client/ScrapingMgmt/services/corporate/sendResults.js
+++ client/ScrapingMgmt/services/corporate/sendResults.js
@@ -4,7 +4,7 @@ app.factory('sendResults',['$http', function($http) {
     var sendLeads = function(resultsToSend) {
         return $http({
             method: 'POST',
-            url: '',
+            url: 'http://localhost:8080/api/scrape/',
             data: resultsToSend
         });
     }
