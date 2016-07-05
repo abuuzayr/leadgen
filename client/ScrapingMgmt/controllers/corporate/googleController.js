3693606a9246395fef519afa50b5282e3664bc99
tanching <tanching@outlook.com>
2016-07-05 12:25:11 +0800

    remove console.log

--- client/ScrapingMgmt/controllers/corporate/googleController.js
+++ client/ScrapingMgmt/controllers/corporate/googleController.js
@@ -44,7 +44,7 @@ app.controller('googleController', ['$scope', 'googleResults', 'ypResults', 'sha
         gc.dataListForGoogle = res.data;
         // console.log('google res is ' + res.data);
     }), function errorCallback(err) {
-        console.log('err is ' + err);
+        // console.log('err is ' + err);
     };
 
     // console.log('getting data from yellow page');
@@ -53,70 +53,62 @@ app.controller('googleController', ['$scope', 'googleResults', 'ypResults', 'sha
         // console.log('yellow page res is ' + res.data);
 
     }), function errorCallback(err) {
-        console.log('err is ' + err);
+        // console.log('err is ' + err);
     };
     
     var stop;
     var count = 0;
 
     gc.transfer = function() {
-        console.log('the server is ' + navigator.onLine);
-        if (angular.isDefined(stop) /*&& stop !== 1*/) {
+        // console.log('the server is ' + navigator.onLine);
+        if (angular.isDefined(stop)) {
             return;
         
         } else if (navigator.onLine === false) {
-            console.log('2.the server is ' + navigator.onLine);
+            // console.log('2.the server is ' + navigator.onLine);
             gc.pauseScraping();
 
         } else if (navigator.onLine === true) {
 
             showInternet(navigator.onLine);
-
-        stop = $interval(function() {
-            if (gc.dataListForGoogle.length > 0 && navigator.onLine === true) {
-                var popLead = gc.dataListForGoogle.pop();
-                gc.gridOptions.data.push(popLead);
-                gc.numScrap = gc.gridOptions.data.length;
-                shareData.addLead(popLead);
-
-            } else if (gc.dataListForYP.length > 0 && navigator.onLine === true) {
-                var popYPLead = gc.dataListForYP.pop();
-                console.log('yp pop lead is ' + popYPLead.firstName);
-                
-                gc.gridOptions.data.push(popYPLead);
-                gc.numScrap = gc.gridOptions.data.length;
-                shareData.addLead(popYPLead);
             
-            // if there is no internet connection, stop scraping, ask for internet
-            } else if (navigator.onLine === false) {
-                console.log('3.the server is ' + navigator.onLine);
-                gc.pauseScraping();
-                showInternet(navigator.onLine);
-
-            } else {
-                console.log('continue scraping');
-                googleResults.continueScrape(gc.category,gc.country).then(function successCallback(res) {
-                    // signal to stop scraping
-                    if (angular.isDefined(res.data.status) && res.data.status === 205) {
-                        gc.stopScraping();
-                        //show the 'view results' button
-                        gc.showFunction();
-                    } else if (angular.isDefined(res.data.status)) {
-                        gc.stopScraping();
-                        gc.showFunction();
-                    } else {
-                        console.log('res is ' + res.data);
-                        gc.dataListForGoogle = res.data;
-                    }     
-            }), function errorCallback(err) {
-                    console.log('err is ' + err);
-                    gc.stopScraping();
-                    //show the 'view results' button
-                    gc.showFunction();
-                };
-            }
-        },2000);
-    };
+            stop = $interval(function() {
+                if (gc.dataListForGoogle.length > 0 && navigator.onLine === true) {
+                    var popLead = gc.dataListForGoogle.pop();
+                    gc.gridOptions.data.push(popLead);
+                    gc.numScrap = gc.gridOptions.data.length;
+                    shareData.addLead(popLead);
+                } else if (gc.dataListForYP.length > 0 && navigator.onLine === true) {
+                    var popYPLead = gc.dataListForYP.pop();
+                    gc.gridOptions.data.push(popYPLead);
+                    gc.numScrap = gc.gridOptions.data.length;
+                    shareData.addLead(popYPLead);
+                
+                // if there is no internet connection, stop scraping, ask for internet
+                } else if (navigator.onLine === false) {
+                    gc.pauseScraping();
+                    showInternet(navigator.onLine);
+                } else {
+                    googleResults.continueScrape(gc.category,gc.country).then(function successCallback(res) {
+                        // signal to stop scraping
+                        if (angular.isDefined(res.data.status) && res.data.status === 205) {
+                            gc.stopScraping();
+                            //show the 'view results' button
+                            gc.showFunction();
+                        } else if (angular.isDefined(res.data.status)) {
+                            gc.stopScraping();
+                            gc.showFunction();
+                        } else {
+                            gc.dataListForGoogle = res.data;
+                        }
+                    }), function errorCallback(err) {
+                        // console.log('err is ' + err);
+                        gc.stopScraping(); 
+                        gc.showFunction(); //show the 'view results' button
+                    };
+                }
+            },2000);
+        };
     }
     
 
