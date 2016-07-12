12bb71fd04d9666dc9b9a89c8b695a7729221067
Nelson Tham <Nelson Tham>
2016-07-12 18:04:38 +0800

    add filter header

--- client/ContactsMgmt/controllers/contactsMainController.js
+++ client/ContactsMgmt/controllers/contactsMainController.js
@@ -125,6 +125,7 @@ app.controller('contactsMainController', ['$scope', '$window', 'leadsData', 'his
             enableFiltering: false,
             enableSorting: false,
             enableEdit: false,
+            headerCellClass: cc.highlightFilteredHeader,
             cellTemplate: '<button class="btn primary" ng-click="grid.appScope.cc.showMe(row.entity)">View</button>'
         }],
         importerDataAddCallback: function(grid, newObjects) {
