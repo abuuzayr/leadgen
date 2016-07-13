3b4a106c1c6daff0bb45ba3dd0878b80784d7c18
Nelson Tham <Nelson Tham>
2016-07-13 17:36:17 +0800

    change to custom dialog

--- client/ContactsMgmt/controllers/mailListController.js
+++ client/ContactsMgmt/controllers/mailListController.js
@@ -118,15 +118,21 @@ mc.showAlert = function() {
     // Appending dialog to document.body to cover sidenav in docs app
     // Modal dialogs should fully cover application
     // to prevent interaction outside of dialog
-    $mdDialog.show(
-      $mdDialog.alert()
-        .parent(angular.element(document.querySelector('#popupContainer')))
-        .clickOutsideToClose(true)
-        .title('This is an alert title')
-        .textContent('You can specify some description text in here.')
-        .ariaLabel('Alert Dialog Demo')
-        .ok('Got it!')
-    );
+    $mdDialog.show({
+      controller: DialogController,
+      contentElement: '#myDialog',
+      parent: angular.element(document.body),
+      clickOutsideToClose: true
+    });
+  };
+
+  function DialogController($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+
+  $scope.cancel = function() {
+    $mdDialog.cancel();
   };
 
   //popup dialog box
