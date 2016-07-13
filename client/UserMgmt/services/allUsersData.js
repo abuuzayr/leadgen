f99740186ec1a9cbe6e6cda5b43022d20d92ed8d
tanching <tanching@outlook.com>
2016-07-13 11:14:45 +0800

    make changes to http get for user management and include delete function

--- client/UserMgmt/services/allUsersData.js
+++ client/UserMgmt/services/allUsersData.js
@@ -1,9 +1,21 @@
-app.factory('allUsersData', ['$http', function($http) { 
-  return $http.get('UserMgmt/testFiles/testData.json')
-    .success(function(data) { 
-        return data; 
-    }) 
-    .error(function(err) { 
-        return err; 
-    }); 
+app.factory('allUsersData', ['$http', function($http) {
+    var getUserData = function() {
+        return $http({
+            method: 'GET',
+            url: 'UserMgmt/testFiles/testData.json'
+        });
+    }
+
+    var deleteUserData = function(usersToRemove) {
+        return $http({
+            method: 'PATCH',
+            url: '',
+            data: usersToRemove
+        })
+    }
+
+    return {
+        getUserData: getUserData,
+        deleteUserData: deleteUserData
+    }
 }]);
