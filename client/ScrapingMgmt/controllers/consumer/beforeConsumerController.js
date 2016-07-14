c63550f1989df55a3bd7885c81cd6d61ccec20b9
tanching <tanching@outlook.com>
2016-07-14 11:39:20 +0800

    format

--- client/ScrapingMgmt/controllers/consumer/beforeConsumerController.js
+++ client/ScrapingMgmt/controllers/consumer/beforeConsumerController.js
@@ -1,19 +1,20 @@
-app.controller('beforeConsumerController', ['$scope', '$http', 'uiGridConstants', '$q', '$location', '$timeout', 'consumerShareInput', 
-                function ($scope, $http, uiGridConstants, $q, $location, $timeout, consumerShareInput) {
-    
-    var beforeConsumer = this;
+app.controller('beforeConsumerController', ['$scope', '$http', 'uiGridConstants', '$q', '$location', '$timeout', 'consumerShareInput',
+    function($scope, $http, uiGridConstants, $q, $location, $timeout, consumerShareInput) {
 
-    beforeConsumer.category = "";
+        var beforeConsumer = this;
 
-    beforeConsumer.setInput = function() {
-        consumerShareInput.setCategory(beforeConsumer.category);
-    }
+        beforeConsumer.category = "";
 
-    beforeConsumer.continue = true;
-    beforeConsumer.continueScraping = function() {
-        if (angular.isDefined(beforeConsumer.category)) {
-            beforeConsumer.continue = false;
+        beforeConsumer.setInput = function() {
+            consumerShareInput.setCategory(beforeConsumer.category);
         }
+
+        beforeConsumer.continue = true;
+        beforeConsumer.continueScraping = function() {
+            if (angular.isDefined(beforeConsumer.category)) {
+                beforeConsumer.continue = false;
+            }
+        }
+
     }
-    
-}]);
+]);
