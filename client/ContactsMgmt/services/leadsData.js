592d0bc78debf59286e14cc8923ac2007136c977
Lim Qi Wen <limqiwen92@gmail.com>
2016-07-15 03:58:35 +0000

    added meta charset in index.html

--- client/ContactsMgmt/services/leadsData.js
+++ client/ContactsMgmt/services/leadsData.js
@@ -1,5 +1,5 @@
 app.factory('leadsData', ['$http', function($http) {  
-    return $http.get('http://10.4.1.145:8080/api/contacts/leadList/leads')  .success(function(data) {    
+    return $http.get('//10.4.1.145:8080/api/contacts/leadList/leads')  .success(function(data) {    
         return data;     
     })    .error(function(err) {       
         return err;
