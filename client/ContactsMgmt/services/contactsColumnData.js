5fbaaa7773e05768ac3a451b6cb14ef2b0a57154
Nelson Tham <Nelson Tham>
2016-07-15 16:09:29 +0800

    change leadsmgmt services api

--- client/ContactsMgmt/services/contactsColumnData.js
+++ client/ContactsMgmt/services/contactsColumnData.js
@@ -1,5 +1,5 @@
-app.factory('contactsColumnData', ['$http', function($http) {  
-	return $http.get('//10.4.1.145/api/contacts/leadList/fields')  .success(function(data) {
+app.factory('contactsColumnData', ['$http', 'appConfig', function($http, appConfig) {  
+	return $http.get(appConfig.API_URL + '/api/contacts/leadList/fields')  .success(function(data) {
 		console.log(data);     
 		return data;    
 	})    .error(function(err) {    
--- client/ContactsMgmt/services/mailListData.js
+++ client/ContactsMgmt/services/mailListData.js
@@ -1,5 +1,5 @@
-app.factory('mailListData', ['$http', function($http) {  
-    return $http.get('//10.4.1.145/api/contacts/mailingList')  .success(function(data) {    
+app.factory('mailListData', ['$http', 'appConfig', function($http, appConfig) {  
+    return $http.get(appConfig.API_URL + '/contacts/mailingList')  .success(function(data) {    
         return data;    
     })    .error(function(err) {        
         return err;    
