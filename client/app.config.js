050360da820fe4274d72925fe697d67145f4e995
Lim Qi Wen <qiwen92@u.nus.edu>
2016-07-15 17:08:34 +0800

    modify ip addr of app server

--- client/app.config.js
+++ client/app.config.js
@@ -1,7 +1,7 @@
 angular.module('app')
 	.constant('appConfig', {
 		APP_VERSION: '0.1',
-		API_URL: 'https://10.4.1.145/api',
+		API_URL: 'https://10.4.1.146/api',
 		AUTH_URL:'https://10.4.1.204/auth/user',
 		FP_URL: 'https://10.4.1.204/forgetpassword/user',
 		MIN_PASSWORD_LENGTH:8,
