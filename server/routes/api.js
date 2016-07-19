705262db6667f2a3189b5a125bcfe6405c241732
an zhi <xiaoturtle93@gmail.com>
2016-07-19 16:18:12 +0800

    verifying testing

--- server/routes/api.js
+++ server/routes/api.js
@@ -34,7 +34,8 @@ apiRouter.use('/',function(req, res, next) {
   console.log(req.url);
   next();
 });
-
+apiRouter.use('*',http403.decodeCookieInfo);
+apiRouter.use('*',http403.decodeAccessInfo);
 
 var leadfinderMgmt = require('./protected/leadfinderMgmt.js');
 var leadlistMgmt = require('./protected/leadlistMgmt.js');
@@ -42,10 +43,7 @@ var cookieGenerator = require('./protected/cookieMgmt.js');
 var dbMgmt = require('./protected/dbMgmt.js');
 
 //PATH
-console.log(1);
-apiRouter.use('*',http403.decodeCookieInfo(req,res,next));
-console.log(2);
-apiRouter.use('*',http403.decodeAccessInfo(req,res,next));
+
 apiRouter.use('/cookie', cookieGenerator);
 apiRouter.use('/', leadfinderMgmt);
 apiRouter.use('/', leadlistMgmt);
