f45898fc7ab9798d1cbff3f009572a62d3319dbb
an zhi <xiaoturtle93@gmail.com>
2016-07-20 14:51:34 +0800

    test stuffs

--- server/utils/403.js
+++ server/utils/403.js
@@ -111,6 +111,7 @@ module.exports = function(){
                				if(err){	
                				    return send403(req,res,err.message);
                				}
+               				req.usertype=usertype;
                				res.cookie('userTypeCookie', token, { maxAge: 360000, httpOnly: false });
                				res.sendStatus(200);
                				});
