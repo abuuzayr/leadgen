baf4187bcffb242b24909d565e1455ba1bd15041
tanching <tanching@outlook.com>
2016-07-20 14:54:29 +0800

    test header 2

--- client/appController.js
+++ client/appController.js
@@ -4,6 +4,53 @@ app.controller('appController', ['$scope', '$q', '$location', '$timeout', 'userS
         var vm = this;
         vm.showLogout = false;
 
+        if (authServices.getToken() && authServices.getUserInfo().usertype === 'User') {
+            vm.showLead = true;
+            vm.showFinder = true;
+            vm.showAccount = true;
+            vm.showUser = false;
+            vm.showDatabase = false;
+            vm.showLogout = true;
+
+            console.log(vm.showLead);
+            console.log(vm.showFinder);
+            console.log(vm.showAccount);
+            console.log(vm.showUser);
+            console.log(vm.showDatabase);
+            console.log(vm.showLogout);
+
+        } else if (authServices.getToken() && authServices.getUserInfo().usertype === 'Admin') {
+            vm.showLead = true;
+            vm.showFinder = true;
+            vm.showAccount = true;
+            vm.showUser = true;
+            vm.showDatabase = false;
+            vm.showLogout = true;
+
+            console.log(vm.showLead);
+            console.log(vm.showFinder);
+            console.log(vm.showAccount);
+            console.log(vm.showUser);
+            console.log(vm.showDatabase);
+            console.log(vm.showLogout);
+
+        } else if (authServices.getToken() && authServices.getUserInfo().usertype === 'SuperAdmin') {
+            vm.showLead = false;
+            vm.showFinder = false;
+            vm.showAccount = false;
+            vm.showUser = false;
+            vm.showDatabase = true;
+            vm.showLogout = true;
+
+            console.log(vm.showLead);
+            console.log(vm.showFinder);
+            console.log(vm.showAccount);
+            console.log(vm.showUser);
+            console.log(vm.showDatabase);
+            console.log(vm.showLogout);
+        }
+
+
         vm.update = function() {
 
             if (authServices.getToken() && authServices.getUserInfo().usertype === 'User') {
@@ -39,6 +86,8 @@ app.controller('appController', ['$scope', '$q', '$location', '$timeout', 'userS
             console.log(vm.showLogout);
         };
 
+        // vm.update();
+
         vm.logout = function() {
             vm.showLead = false;
             vm.showFinder = false;
@@ -58,7 +107,6 @@ app.controller('appController', ['$scope', '$q', '$location', '$timeout', 'userS
         });
         viewContentLoaded.promise.then(function() {
             $timeout(function() {
-                vm.update();
                 componentHandler.upgradeDom();
             }, 0);
         });
