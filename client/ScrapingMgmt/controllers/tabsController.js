dbfd375c521bf34a981db9baac293e18181fd47b
tanching <tanching@outlook.com>
2016-07-21 11:57:04 +0800

    change the if else for tabs controller

--- client/ScrapingMgmt/controllers/tabsController.js
+++ client/ScrapingMgmt/controllers/tabsController.js
@@ -2,15 +2,15 @@ angular
     .module("app")
     .controller("tabsController", ['authServices', '$scope', function(authServices, $scope) {
         var vm = this;
-        vm.showCorporate = true;
-        vm.showConsumer = true;
+        vm.showCorporate = false;
+        vm.showConsumer = false;
         console.log(authServices.getUserInfo().subType);
 
-        if (authServices.getToken() && authServices.getUserInfo().subType.consumer === false) {
-            vm.showConsumer = false;
+        if (authServices.getToken() && authServices.getUserInfo().subType.consumer === true) {
+            vm.showConsumer = true;
             console.log(vm.showCorporate);
-        } else if (authServices.getToken() && authServices.getUserInfo().subType.corporate === false) {
-            vm.showCorporate = false;
+        } else if (authServices.getToken() && authServices.getUserInfo().subType.corporate === true) {
+            vm.showCorporate = true;
             console.log(vm.showConsumer);
         }
 
