0f585576afdfecd036dd1f873f1d7df9b31e0217
Lim Qi Wen <limqiwen92@gmail.com>
2016-07-25 08:07:00 +0000

    fix acccountsettingsMgmt.js bug

--- server/routes/protected/accountsettingsMgmt.js
+++ server/routes/protected/accountsettingsMgmt.js
@@ -3,7 +3,6 @@ var accountsettingsRouter = express.Router();
 var request = require('request');
 var http403 = require('../../utils/403')();
 
-
 //ACCESS CONTROL
 accountsettingsRouter.use('*',http403.verifyAccess('accountsetting'));
 
@@ -11,17 +10,19 @@ accountsettingsRouter.use('*',http403.verifyAccess('accountsetting'));
 accountsettingsRouter.route('/')
 .get(function(req,res){
   var coId = req.decoded.companyId;
-  var cookie = req.cookie.session;
+  var cookie = req.cookies.session;
   console.log("======Cookie is ========");
   console.log(cookie);
   request({
-            url:'//10.4.1.198/api/usermgmt',
+            url:'https://10.4.1.198/api/usermgmt',
             method:'GET',
             json:true,
             jar:true
           },function(err,response,body ){
-    if(err)
+    if(err){
+      console.log(err);
       res.sendStatus(500);
+    }
     else
     res.json(body);
 
