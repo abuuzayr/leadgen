1965907a9d22950ed611172ab16f509334e5e83b
tanching <tanching@outlook.com>
2016-07-25 15:00:16 +0800

    change api

--- client/app.config.js
+++ client/app.config.js
@@ -1,7 +1,8 @@
 angular.module('app')
     .constant('appConfig', {
         APP_VERSION: '0.1',
-        API_URL: '//10.4.1.145/api',
+        // API_URL: '//10.4.1.145/api',
+        API_URL: '//localhost:8080/api',
         AUTH_URL: '//10.4.1.145/auth/user',
         FP_URL: '//10.4.1.145/forgetpassword/user',
         UM_URL: '//10.4.1.145/api/usermgmt',
