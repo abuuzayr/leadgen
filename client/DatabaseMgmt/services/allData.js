262076f24f08b7031440413c21f2ca1096f8598b
tanching <tanching@outlook.com>
2016-07-29 18:41:50 +0800

    function strict for allData service

--- client/DatabaseMgmt/services/allData.js
+++ client/DatabaseMgmt/services/allData.js
@@ -1,30 +1,24 @@
-app.factory('allData', ['$http', 'appConfig', function($http, appConfig) {  
-    var getAllLeads = function() {
-        return $http({
-            method: 'GET',
-            url: appConfig.API_URL + '/dbmgmt/all'
-        });
-    };
+(function() {
+    "use strict";
+    app.factory('allData', ['$http', 'appConfig', function($http, appConfig) {  
+        var getAllLeads = function() {
+            return $http({
+                method: 'GET',
+                url: appConfig.API_URL + '/dbmgmt/all'
+            });
+        };
 
-    var deleteAllLeads = function(leadsToDelete) {
-        return $http({
-            method: 'PUT',
-            url: appConfig.API_URL + '/dbmgmt/all',
-            data: leadsToDelete
-        });
-    };
+        var deleteAllLeads = function(leadsToDelete) {
+            return $http({
+                method: 'PUT',
+                url: appConfig.API_URL + '/dbmgmt/all',
+                data: leadsToDelete
+            });
+        };
 
-    // var editAllLeads = function(leadsToEdit) {
-    //     return $http({
-    //         method: 'PATCH',
-    //         url: '',
-    //         data: leadsToEdit
-    //     });
-    // }
-
-    return {
-        getAllLeads: getAllLeads,
-        deleteAllLeads: deleteAllLeads,
-        // editAllLeads: editAllLeads
-    };
-}]);
+        return {
+            getAllLeads: getAllLeads,
+            deleteAllLeads: deleteAllLeads,
+        };
+    }]);
+})();
