f481d6daa5cb16f172711446529158f496cab540
tanching <tanching@outlook.com>
2016-07-29 18:50:01 +0800

    function strict syncToCompany

--- client/DatabaseMgmt/services/syncToCompany.js
+++ client/DatabaseMgmt/services/syncToCompany.js
@@ -1,15 +1,18 @@
-//send results to backend to sync to companies
-app.factory('syncToCompany', ['$http', function($http) {
+(function() {
+    "use strict";
+    //send results to backend to sync to companies
+    app.factory('syncToCompany', ['$http', function($http) {
 
-    var sendToCompany = function(resultsToSend) {
-        return $http({
-            method: 'POST',
-            url: '',
-            data: resultsToSend
-        });
-    };
+        var sendToCompany = function(resultsToSend) {
+            return $http({
+                method: 'POST',
+                url: '',
+                data: resultsToSend
+            });
+        };
 
-    return {
-        sendToCompany: sendToCompany
-    };
-}]);
+        return {
+            sendToCompany: sendToCompany
+        };
+    }]);
+})();
