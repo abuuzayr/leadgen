4ddd806458a6c407fee05c5c77fdbd837aacfe1b
Lim Qi Wen <qiwen92@u.nus.edu>
2016-07-29 15:04:27 +0800

    split syncing and showing leads api

---
+++ client/ContactsMgmt/services/syncData.js
@@ -0,0 +1,7 @@
+app.factory('syncData', ['$http', 'appConfig', function($http, appConfig) {  
+    return $http.get(appConfig.API_URL + '/contacts/sync')  .success(function(data) {    
+        return data;     
+    })    .error(function(err) {       
+        return err;
+    });
+}]);
--- client/app.config.js
+++ client/app.config.js
@@ -1,10 +1,10 @@
 angular.module('app')
     .constant('appConfig', {
         APP_VERSION: '0.1',
-        API_URL: '//10.4.1.213/api',
-        AUTH_URL: '//10.4.1.213/auth/user',
-        FP_URL: '//10.4.1.213/forgetpassword/user',
-        UM_URL: '//10.4.1.213/usermgmt',
+        API_URL: '//10.4.1.145/api',
+        AUTH_URL: '//10.4.1.145/auth/user',
+        FP_URL: '//10.4.1.145/forgetpassword/user',
+        UM_URL: '//10.4.1.145/usermgmt',
         MIN_PASSWORD_LENGTH: 8,
         MAX_PASSWORD_LENGTH: 24
     });
