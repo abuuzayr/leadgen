f7142b00be053bd6e65751c450f199f610385165
tanching <tanching@outlook.com>
2016-07-29 18:45:11 +0800

    function strict for local data service

--- client/DatabaseMgmt/services/localData.js
+++ client/DatabaseMgmt/services/localData.js
@@ -1,49 +1,51 @@
-app.factory('localData', ['$http', 'appConfig', function($http, appConfig) {  
-    var getLocalLeads = function() {
-        return $http({
-            method: 'GET',
-            url: appConfig.API_URL + '/dbmgmt/local'
-        });
-    };
+(function() {
+    "use strict";
+    app.factory('localData', ['$http', 'appConfig', function($http, appConfig) {  
+        var getLocalLeads = function() {
+            return $http({
+                method: 'GET',
+                url: appConfig.API_URL + '/dbmgmt/local'
+            });
+        };
 
-    var deleteLocalLeads = function(leadsToDelete) {
-        return $http({
-            method: 'PUT',
-            url: appConfig.API_URL + '/dbmgmt/local',
-            data: leadsToDelete
-        });
-    };
+        var deleteLocalLeads = function(leadsToDelete) {
+            return $http({
+                method: 'PUT',
+                url: appConfig.API_URL + '/dbmgmt/local',
+                data: leadsToDelete
+            });
+        };
 
-    var editLocalLeads = function(leadsToEdit) {
-        return $http({
-            method: 'PATCH',
-            url: appConfig.API_URL + '/dbmgmt/local',
-            data: leadsToEdit
-        });
-    };
+        var editLocalLeads = function(leadsToEdit) {
+            return $http({
+                method: 'PATCH',
+                url: appConfig.API_URL + '/dbmgmt/local',
+                data: leadsToEdit
+            });
+        };
 
-    //send object 
-    var importToLocal = function(importData) {
-        return $http({
-            method: 'POST',
-            url: appConfig.API_URL + '/dbmgmt/local',
-            data: importData
-        });
-    };
+        //send object 
+        var importToLocal = function(importData) {
+            return $http({
+                method: 'POST',
+                url: appConfig.API_URL + '/dbmgmt/local',
+                data: importData
+            });
+        };
 
-    var syncFromExternalLeads = function() {
-        return $http({
-            method: 'GET',
-            url: appConfig.API_URL + '/dbmgmt/local/import',
-        });
-    };
+        var syncFromExternalLeads = function() {
+            return $http({
+                method: 'GET',
+                url: appConfig.API_URL + '/dbmgmt/local/import',
+            });
+        };
 
-    return {
-        getLocalLeads: getLocalLeads,
-        deleteLocalLeads: deleteLocalLeads,
-        editLocalLeads: editLocalLeads,
-        importToLocal: importToLocal,
-        syncFromExternalLeads: syncFromExternalLeads
-    };
-
-}]);
+        return {
+            getLocalLeads: getLocalLeads,
+            deleteLocalLeads: deleteLocalLeads,
+            editLocalLeads: editLocalLeads,
+            importToLocal: importToLocal,
+            syncFromExternalLeads: syncFromExternalLeads
+        };
+    }]);
+})();
