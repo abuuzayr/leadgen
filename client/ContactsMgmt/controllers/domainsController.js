ded33f3177642e47870954b7a9e3a52ba26fbd2d
tanching <tanching@outlook.com>
2016-08-01 10:26:05 +0800

    add function strict for contacts controller

--- client/ContactsMgmt/controllers/domainsController.js
+++ client/ContactsMgmt/controllers/domainsController.js
@@ -1,129 +1,134 @@
-app.controller('domainsController', ['$scope', 'appConfig', '$window', 'domainsData', '$http', '$interval', 'uiGridConstants', '$q', '$location', '$timeout', function($scope, appConfig, $window, domainsData, $http, $interval, uiGridConstants, $q, $location, $timeout) {
+(function() {
+    'use strict';
+    app.controller('domainsController', ['$scope', 'appConfig', '$window', 'domainsData', '$http', '$interval', 'uiGridConstants', '$q', '$location', '$timeout',
+        function($scope, appConfig, $window, domainsData, $http, $interval, uiGridConstants, $q, $location, $timeout) {
 
-    var dc = this;
+            var dc = this;
 
-    /** Get the blocked domains from database */
-    domainsData.success(function(data) {
-        dc.gridOptions.data = data;
-    });
-
-    var viewContentLoaded = $q.defer();
-    $scope.$on('$viewContentLoaded', function() {
-        $timeout(function() {
-            viewContentLoaded.resolve();
-        }, 0);
-    });
-    viewContentLoaded.promise.then(function() {
-        $timeout(function() {
-            componentHandler.upgradeDom();
-        }, 0);
-    });
-
-    dc.highlightFilteredHeader = function(row, rowRenderIndex, col, colRenderIndex) {
-        if (col.filters[0].term) {
-            return 'header-filtered';
-        } else {
-            return '';
-        }
-    };
-
-    /** UI Grid for blocked domains */
-    dc.gridOptions = {
-        showGridFooter: true,
-        enableFiltering: true,
-        enableSorting: true,
-        minRowsToShow: 4,
-        columnDefs: [{
-            field: 'domain',
-            displayName: 'Domain',
-            enableCellEdit: true,
-            headerCellClass: dc.highlightFilteredHeader
-        }],
-    };
-
-    /** 
-     * This method refreshes the page. 
-     * It is used to update the data shown in UI-Grid
-     */
-    dc.refresh = function() {
-        $window.location.reload();
-    };
-
-    /** 
-     * Adds a new domain into the UI-Grid
-     * http.post - adds the new domain to the database and refresh the page
-     * @param {Object} domain 
-     * */
-    dc.addDomain = function() {
-        var domain = dc.domainSelected;
-        var arrName = domain.split(" ");
-        var editedDomain = "";
-        for (var x of arrName) {
-            if (x !== "") {
-                editedDomain += x;
-            }
-        }
-        dc.gridOptions.data.push({
-            "domain": editedDomain
-        });
-
-        var domain = {
-            "domain": editedDomain
-        };
-        var url = "/contacts/blackList/domain";
-        $http.post(appConfig.API_URL + url, domain)
-            .then(function(res) {
-                $window.location.reload();
+            /** Get the blocked domains from database */
+            domainsData.success(function(data) {
+                dc.gridOptions.data = data;
             });
-    };
 
-    /**
-     * This method creates a new variable to store the data of the domain to be deleted. 
-     * It is retrieve using ng-model. 
-     * This method is called so as to facilitate the confirmation of delete via popup dialog.
-     */
-    dc.selectDeleteDomain = function() {
-        dc.selectedDeleteDomain = dc.domainSelected;
-    };
+            var viewContentLoaded = $q.defer();
+            $scope.$on('$viewContentLoaded', function() {
+                $timeout(function() {
+                    viewContentLoaded.resolve();
+                }, 0);
+            });
+            viewContentLoaded.promise.then(function() {
+                $timeout(function() {
+                    componentHandler.upgradeDom();
+                }, 0);
+            });
 
-    /**
-     * This method deletes a domain based on the data binded to the variable created when 
-     * selectDeleteDomain() method is called.
-     * http.put - deletes domain from database and refreshes page if successful
-     */
-    dc.deleteDomain = function() {
-        for (var x in dc.gridOptions.data) {
-            if ((dc.gridOptions.data[x].domain === dc.selectedDeleteDomain)) {
-                var domain = dc.gridOptions.data.splice(x, 1);
-                var url = "/contacts/blackList/domain";
-                var deleteStatus = $http.put(appConfig.API_URL + url, domain[0]);
+            dc.highlightFilteredHeader = function(row, rowRenderIndex, col, colRenderIndex) {
+                if (col.filters[0].term) {
+                    return 'header-filtered';
+                } else {
+                    return '';
+                }
+            };
+
+            /** UI Grid for blocked domains */
+            dc.gridOptions = {
+                showGridFooter: true,
+                enableFiltering: true,
+                enableSorting: true,
+                minRowsToShow: 4,
+                columnDefs: [{
+                    field: 'domain',
+                    displayName: 'Domain',
+                    enableCellEdit: true,
+                    headerCellClass: dc.highlightFilteredHeader
+                }],
+            };
+
+            /** 
+             * This method refreshes the page. 
+             * It is used to update the data shown in UI-Grid
+             */
+            dc.refresh = function() {
                 $window.location.reload();
-            }
+            };
+
+            /** 
+             * Adds a new domain into the UI-Grid
+             * http.post - adds the new domain to the database and refresh the page
+             * @param {Object} domain 
+             * */
+            dc.addDomain = function() {
+                var domain = dc.domainSelected;
+                var arrName = domain.split(" ");
+                var editedDomain = "";
+                for (var x of arrName) {
+                    if (x !== "") {
+                        editedDomain += x;
+                    }
+                }
+                dc.gridOptions.data.push({
+                    "domain": editedDomain
+                });
+
+                var domain = {
+                    "domain": editedDomain
+                };
+                var url = "/contacts/blackList/domain";
+                $http.post(appConfig.API_URL + url, domain)
+                    .then(function(res) {
+                        $window.location.reload();
+                    });
+            };
+
+            /**
+             * This method creates a new variable to store the data of the domain to be deleted. 
+             * It is retrieve using ng-model. 
+             * This method is called so as to facilitate the confirmation of delete via popup dialog.
+             */
+            dc.selectDeleteDomain = function() {
+                dc.selectedDeleteDomain = dc.domainSelected;
+            };
+
+            /**
+             * This method deletes a domain based on the data binded to the variable created when 
+             * selectDeleteDomain() method is called.
+             * http.put - deletes domain from database and refreshes page if successful
+             */
+            dc.deleteDomain = function() {
+                for (var x in dc.gridOptions.data) {
+                    if ((dc.gridOptions.data[x].domain === dc.selectedDeleteDomain)) {
+                        var domain = dc.gridOptions.data.splice(x, 1);
+                        var url = "/contacts/blackList/domain";
+                        var deleteStatus = $http.put(appConfig.API_URL + url, domain[0]);
+                        $window.location.reload();
+                    }
+                }
+            };
+
+            dc.gridOptions.onRegisterApi = function(gridApi) {
+                dc.gridApi = gridApi;
+                //save after edit
+                gridApi.edit.on.afterCellEdit($scope, function(rowEntity, colDef, newValue, oldValue) {
+                    console.log('edited row id:' + rowEntity.firstName + ' Column:' + colDef.name + ' newValue:' + newValue + ' oldValue:' + oldValue);
+                    $scope.$apply();
+                    $window.location.reload();
+                });
+            };
+
+            //popup dialog box
+            dc.openDialog = function(dialogName) {
+                var dialog = document.querySelector('#' + dialogName);
+                if (!dialog.showModal) {
+                    dialogPolyfill.registerDialog(dialog);
+                }
+                dialog.showModal();
+            };
+
+            dc.closeDialog = function(dialogName) {
+                var dialog = document.querySelector('#' + dialogName);
+                dialog.close();
+            };
+
         }
-    };
-
-    dc.gridOptions.onRegisterApi = function(gridApi) {
-        dc.gridApi = gridApi;
-        //save after edit
-        gridApi.edit.on.afterCellEdit($scope, function(rowEntity, colDef, newValue, oldValue) {
-            console.log('edited row id:' + rowEntity.firstName + ' Column:' + colDef.name + ' newValue:' + newValue + ' oldValue:' + oldValue);
-            $scope.$apply();
-            $window.location.reload();
-        });
-    };
-
-    //popup dialog box
-    dc.openDialog = function(dialogName) {
-        var dialog = document.querySelector('#' + dialogName);
-        if (!dialog.showModal) {
-            dialogPolyfill.registerDialog(dialog);
-        }
-        dialog.showModal();
-    };
-
-    dc.closeDialog = function(dialogName) {
-        var dialog = document.querySelector('#' + dialogName);
-        dialog.close();
-    };
-
-}]);
+    ]);
+})();
--- client/ContactsMgmt/controllers/viewMailListController.js
+++ client/ContactsMgmt/controllers/viewMailListController.js
@@ -1,231 +1,234 @@
-app.controller('viewMailListController', ['$scope', 'appConfig', '$window', 'detailedMailListData', 'shareMailList', '$http', '$interval', 'uiGridConstants', '$q', '$location', '$timeout', function($scope, appConfig, $window, detailedMailListData, shareMailList, $http, $interval, uiGridConstants, $q, $location, $timeout) {
+(function() {
+    'use strict';
+    app.controller('viewMailListController', ['$scope', 'appConfig', '$window', 'detailedMailListData', 'shareMailList', '$http', '$interval', 'uiGridConstants', '$q', '$location', '$timeout', function($scope, appConfig, $window, detailedMailListData, shareMailList, $http, $interval, uiGridConstants, $q, $location, $timeout) {
 
-    var vmc = this;
+        var vmc = this;
 
-    vmc.mailListResult = shareMailList.getData();
+        vmc.mailListResult = shareMailList.getData();
 
-    detailedMailListData.getMailListData(vmc.mailListResult).then(function successCallback(res) {
-            console.log(res);
-            vmc.gridOptions.data = res.data;
-        }),
-        function errorCallback(err) {
-            console.log('err is ' + err);
+        detailedMailListData.getMailListData(vmc.mailListResult).then(function successCallback(res) {
+                console.log(res);
+                vmc.gridOptions.data = res.data;
+            }),
+            function errorCallback(err) {
+                console.log('err is ' + err);
+            };
+
+        var viewContentLoaded = $q.defer();
+        $scope.$on('$viewContentLoaded', function() {
+            $timeout(function() {
+                viewContentLoaded.resolve();
+            }, 0);
+        });
+        viewContentLoaded.promise.then(function() {
+            $timeout(function() {
+                componentHandler.upgradeDom();
+            }, 0);
+        });
+
+        vmc.highlightFilteredHeader = function(row, rowRenderIndex, col, colRenderIndex) {
+            if (col.filters[0].term) {
+                return 'header-filtered';
+            } else {
+                return '';
+            }
         };
 
-    var viewContentLoaded = $q.defer();
-    $scope.$on('$viewContentLoaded', function() {
-        $timeout(function() {
-            viewContentLoaded.resolve();
-        }, 0);
-    });
-    viewContentLoaded.promise.then(function() {
-        $timeout(function() {
-            componentHandler.upgradeDom();
-        }, 0);
-    });
-
-    vmc.highlightFilteredHeader = function(row, rowRenderIndex, col, colRenderIndex) {
-        if (col.filters[0].term) {
-            return 'header-filtered';
-        } else {
-            return '';
-        }
-    };
-
-    vmc.gridOptions = {
-        enableSorting: true,
-        enableFiltering: true,
-        showGridFooter: true,
-        enableCellEdit: false,
-        columnDefs: [{
-            field: 'firstName',
-            displayName: 'First Name',
-            minWidth: 80,
-            width: 150,
-            headerCellClass: vmc.highlightFilteredHeader
-        }, {
-            field: 'lastName',
-            displayName: 'Last Name',
-            minWidth: 80,
-            width: 150,
-            headerCellClass: vmc.highlightFilteredHeader
-        }, {
-            field: 'company',
-            displayName: 'Company',
-            minWidth: 80,
-            width: 150,
-            headerCellClass: vmc.highlightFilteredHeader
-        }, {
-            field: 'email',
-            displayName: 'Email',
-            minWidth: 80,
-            width: 200,
-            headerCellClass: vmc.highlightFilteredHeader
-        }, {
-            field: 'phone',
-            displayName: 'Phone',
-            minWidth: 80,
-            width: 150,
-            headerCellClass: vmc.highlightFilteredHeader
-        }, {
-            field: 'category',
-            displayName: 'Category',
-            minWidth: 80,
-            width: 120,
-            headerCellClass: vmc.highlightFilteredHeader
-        }, {
-            field: "type",
-            displayName: "Type",
-            editableCellTemplate: "ui-grid/dropdownEditor",
-            minWidth: 80,
-            width: 120,
-            filter: {
-                type: uiGridConstants.filter.SELECT,
-                selectOptions: [{
-                    value: "1",
-                    label: "Corporate"
+        vmc.gridOptions = {
+            enableSorting: true,
+            enableFiltering: true,
+            showGridFooter: true,
+            enableCellEdit: false,
+            columnDefs: [{
+                field: 'firstName',
+                displayName: 'First Name',
+                minWidth: 80,
+                width: 150,
+                headerCellClass: vmc.highlightFilteredHeader
+            }, {
+                field: 'lastName',
+                displayName: 'Last Name',
+                minWidth: 80,
+                width: 150,
+                headerCellClass: vmc.highlightFilteredHeader
+            }, {
+                field: 'company',
+                displayName: 'Company',
+                minWidth: 80,
+                width: 150,
+                headerCellClass: vmc.highlightFilteredHeader
+            }, {
+                field: 'email',
+                displayName: 'Email',
+                minWidth: 80,
+                width: 200,
+                headerCellClass: vmc.highlightFilteredHeader
+            }, {
+                field: 'phone',
+                displayName: 'Phone',
+                minWidth: 80,
+                width: 150,
+                headerCellClass: vmc.highlightFilteredHeader
+            }, {
+                field: 'category',
+                displayName: 'Category',
+                minWidth: 80,
+                width: 120,
+                headerCellClass: vmc.highlightFilteredHeader
+            }, {
+                field: "type",
+                displayName: "Type",
+                editableCellTemplate: "ui-grid/dropdownEditor",
+                minWidth: 80,
+                width: 120,
+                filter: {
+                    type: uiGridConstants.filter.SELECT,
+                    selectOptions: [{
+                        value: "1",
+                        label: "Corporate"
+                    }, {
+                        value: "2",
+                        label: "Consumer"
+                    }]
+                },
+                cellFilter: "mapType",
+                editDropdownValueLabel: "type",
+                headerCellClass: vmc.highlightFilteredHeader,
+                editDropdownOptionsArray: [{
+                    id: 1,
+                    type: "Corporate"
                 }, {
-                    value: "2",
-                    label: "Consumer"
+                    id: 2,
+                    type: "Consumer"
                 }]
-            },
-            cellFilter: "mapType",
-            editDropdownValueLabel: "type",
-            headerCellClass: vmc.highlightFilteredHeader,
-            editDropdownOptionsArray: [{
-                id: 1,
-                type: "Corporate"
             }, {
-                id: 2,
-                type: "Consumer"
-            }]
-        }, {
-            field: 'status',
-            displayName: 'Status',
-            editableCellTemplate: "ui-grid/dropdownEditor",
-            minWidth: 80,
-            width: 150,
-            filter: {
-                type: uiGridConstants.filter.SELECT,
-                selectOptions: [{
-                    value: '1',
-                    label: 'Subscribed'
+                field: 'status',
+                displayName: 'Status',
+                editableCellTemplate: "ui-grid/dropdownEditor",
+                minWidth: 80,
+                width: 150,
+                filter: {
+                    type: uiGridConstants.filter.SELECT,
+                    selectOptions: [{
+                        value: '1',
+                        label: 'Subscribed'
+                    }, {
+                        value: '2',
+                        label: 'Unsubscribed'
+                    }, {
+                        value: '3',
+                        label: 'Cleaned'
+                    }, {
+                        value: '4',
+                        label: 'Pending'
+                    }]
+                },
+                cellFilter: 'mapStatus',
+                editDropdownValueLabel: "status",
+                headerCellClass: vmc.highlightFilteredHeader,
+                editDropdownOptionsArray: [{
+                    id: 1,
+                    status: "Subscribed"
                 }, {
-                    value: '2',
-                    label: 'Unsubscribed'
+                    id: 2,
+                    status: "Unsubscribed"
                 }, {
-                    value: '3',
-                    label: 'Cleaned'
+                    id: 3,
+                    status: "Cleaned"
                 }, {
-                    value: '4',
-                    label: 'Pending'
+                    id: 4,
+                    status: "Pending"
                 }]
-            },
-            cellFilter: 'mapStatus',
-            editDropdownValueLabel: "status",
-            headerCellClass: vmc.highlightFilteredHeader,
-            editDropdownOptionsArray: [{
-                id: 1,
-                status: "Subscribed"
-            }, {
-                id: 2,
-                status: "Unsubscribed"
-            }, {
-                id: 3,
-                status: "Cleaned"
-            }, {
-                id: 4,
-                status: "Pending"
-            }]
-        }],
-    };
+            }],
+        };
 
-    //refresh
-    vmc.refresh = function() {
-        $window.location.reload();
-    };
+        //refresh
+        vmc.refresh = function() {
+            $window.location.reload();
+        };
 
-    /**
-     * Deletes the selected leads
-     * Limits the number of leads to be deleted to be 10 due to mailchimp only allowing 10 continuous connection
-     * Will open dialog if user tries to delete more than 10 leads.
-     * http.put - delete leads from database
-     * @param {array} leads - The selected leads to be deleted
-     */
-    vmc.deleteSelected = function() {
+        /**
+         * Deletes the selected leads
+         * Limits the number of leads to be deleted to be 10 due to mailchimp only allowing 10 continuous connection
+         * Will open dialog if user tries to delete more than 10 leads.
+         * http.put - delete leads from database
+         * @param {array} leads - The selected leads to be deleted
+         */
+        vmc.deleteSelected = function() {
 
-        var count = vmc.gridApi.selection.getSelectedRows().length;
+            var count = vmc.gridApi.selection.getSelectedRows().length;
 
-        if (count <= 10) {
-            angular.forEach(vmc.gridApi.selection.getSelectedRows(), function(data, index) {
-                vmc.gridOptions.data.splice(vmc.gridOptions.data.lastIndexOf(data), 1);
-            });
+            if (count <= 10) {
+                angular.forEach(vmc.gridApi.selection.getSelectedRows(), function(data, index) {
+                    vmc.gridOptions.data.splice(vmc.gridOptions.data.lastIndexOf(data), 1);
+                });
 
-            var leads = vmc.gridApi.selection.getSelectedRows();
-            var url = "/contacts/mailingList/subscriber";
-            $http.put(appConfig.API_URL + url, leads).then(function successCallback(res) {
+                var leads = vmc.gridApi.selection.getSelectedRows();
+                var url = "/contacts/mailingList/subscriber";
+                $http.put(appConfig.API_URL + url, leads).then(function successCallback(res) {
+                    $window.location.reload();
+                });
+            } else if (count > 10) {
+                vmc.openDialog('deleteLimit');
+            }
+        };
+
+        vmc.gridOptions.onRegisterApi = function(gridApi) {
+            vmc.gridApi = gridApi;
+            //save after edit
+            gridApi.edit.on.afterCellEdit($scope, function(rowEntity, colDef, newValue, oldValue) {
+                console.log('edited row id:' + rowEntity.firstName + ' Column:' + colDef.name + ' newValue:' + newValue + ' oldValue:' + oldValue);
+                $scope.$apply();
                 $window.location.reload();
             });
-        } else if (count > 10) {
-            vmc.openDialog('deleteLimit');
-        }
-    };
+        };
 
-    vmc.gridOptions.onRegisterApi = function(gridApi) {
-        vmc.gridApi = gridApi;
-        //save after edit
-        gridApi.edit.on.afterCellEdit($scope, function(rowEntity, colDef, newValue, oldValue) {
-            console.log('edited row id:' + rowEntity.firstName + ' Column:' + colDef.name + ' newValue:' + newValue + ' oldValue:' + oldValue);
-            $scope.$apply();
-            $window.location.reload();
-        });
-    };
+        //popup dialog box
+        vmc.openDialog = function(dialogName) {
+            var dialog = document.querySelector('#' + dialogName);
+            if (!dialog.showModal) {
+                dialogPolyfill.registerDialog(dialog);
+            }
+            dialog.showModal();
+        };
 
-    //popup dialog box
-    vmc.openDialog = function(dialogName) {
-        var dialog = document.querySelector('#' + dialogName);
-        if (!dialog.showModal) {
-            dialogPolyfill.registerDialog(dialog);
-        }
-        dialog.showModal();
-    };
+        vmc.closeDialog = function(dialogName) {
+            var dialog = document.querySelector('#' + dialogName);
+            dialog.close();
+        };
+    }])
 
-    vmc.closeDialog = function(dialogName) {
-        var dialog = document.querySelector('#' + dialogName);
-        dialog.close();
-    };
-}])
+    //filter drop down option hashing
+    .filter('mapStatus', function() {
+        var statusHash = {
+            1: 'Subscribed',
+            2: 'Unsubscribed',
+            3: 'Cleaned',
+            4: 'Pending'
+        };
 
-//filter drop down option hashing
-.filter('mapStatus', function() {
-    var statusHash = {
-        1: 'Subscribed',
-        2: 'Unsubscribed',
-        3: 'Cleaned',
-        4: 'Pending'
-    };
+        return function(input) {
+            if (!input) {
+                return 'error';
+            } else {
+                return statusHash[input];
+            }
+        };
+    })
 
-    return function(input) {
-        if (!input) {
-            return 'error';
-        } else {
-            return statusHash[input];
-        }
-    };
-})
+    //filter drop down option hashing
+    .filter('mapType', function() {
+        var typeHash = {
+            1: 'Corporate',
+            2: 'Consumer'
+        };
 
-//filter drop down option hashing
-.filter('mapType', function() {
-    var typeHash = {
-        1: 'Corporate',
-        2: 'Consumer'
-    };
-
-    return function(input) {
-        if (!input) {
-            return 'Unselected';
-        } else {
-            return typeHash[input];
-        }
-    };
-});
+        return function(input) {
+            if (!input) {
+                return 'Unselected';
+            } else {
+                return typeHash[input];
+            }
+        };
+    });
+})();
