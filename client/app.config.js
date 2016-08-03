741677c01bc9ca8df2a98389882a3c0ec1151ec7
Hellyna NG <hellyna@groventure.com>
2016-08-03 14:41:27 +0800

    Change bulletleads api server location.

--- client/app.config.js
+++ client/app.config.js
@@ -1,7 +1,7 @@
 angular.module('app')
     .constant('appConfig', {
         APP_VERSION: '0.1',
-        API_URL: '//10.4.1.213/api',
+        API_URL: '//app.bulletleads.com/api',
         AUTH_URL: '//app.bulletsuite.com/api/auth/user',
         FP_URL: '//app.bulletsuite.com/api/forgetpassword/user',
         UM_URL: '//app.bulletsuite.com/api/usermgmt',
