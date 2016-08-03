48d4c7155792530a881ea01217d0920e82777086
Hellyna NG <hellyna@groventure.com>
2016-08-03 12:07:15 +0800

    Fixed connection URLs for production.

--- client/app.config.js
+++ client/app.config.js
@@ -2,12 +2,9 @@ angular.module('app')
     .constant('appConfig', {
         APP_VERSION: '0.1',
         API_URL: '//10.4.1.213/api',
-        AUTH_URL: '//10.4.1.213/auth/user',
-        FP_URL: '//10.4.1.213/forgetpassword/user',
-        UM_URL: '//10.4.1.213/usermgmt',
-//        AUTH_URL: '//app.bulletsuite.com/api/auth/user',
-//        FP_URL: '//app.bulletsuite.com/api/forgetpassword/user',
-//        UM_URL: '//app.bulletsuite.com/api/usermgmt',
+        AUTH_URL: '//app.bulletsuite.com/api/auth/user',
+        FP_URL: '//app.bulletsuite.com/api/forgetpassword/user',
+        UM_URL: '//app.bulletsuite.com/api/usermgmt',
         MIN_PASSWORD_LENGTH: 8,
         MAX_PASSWORD_LENGTH: 24
     });
