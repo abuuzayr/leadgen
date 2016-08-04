d94752eb6615c768702f721f25131c7ddfe4d3f5
tanching <tanching@outlook.com>
2016-08-04 14:40:29 +0800

    remove unused bootstrap, change UM_URL

--- client/app.config.js
+++ client/app.config.js
@@ -4,10 +4,11 @@ angular.module('app')
         API_URL: '//10.4.1.213/api',
         AUTH_URL: '//10.4.1.213/auth/user',
         FP_URL: '//10.4.1.213/forgetpassword/user',
-        UM_URL: '//10.4.1.213/usermgmt',
-//        AUTH_URL: '//app.bulletsuite.com/api/auth/user',
-//        FP_URL: '//app.bulletsuite.com/api/forgetpassword/user',
-//        UM_URL: '//app.bulletsuite.com/api/usermgmt',
+        UM_URL: '//10.4.1.198/req/api/usermgmt',
+        // UM_URL: '//10.4.1.213/usermgmt',
+        //        AUTH_URL: '//app.bulletsuite.com/api/auth/user',
+        //        FP_URL: '//app.bulletsuite.com/api/forgetpassword/user',
+        //        UM_URL: '//app.bulletsuite.com/api/usermgmt',
         MIN_PASSWORD_LENGTH: 8,
         MAX_PASSWORD_LENGTH: 24
     });
