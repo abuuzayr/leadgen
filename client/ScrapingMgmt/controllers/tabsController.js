53087f2fd8ee20cee31bbe046d7d41be65afcff3
tanching <tanching@outlook.com>
2016-08-05 14:45:09 +0800

    use ng disable

--- client/ScrapingMgmt/controllers/tabsController.js
+++ client/ScrapingMgmt/controllers/tabsController.js
@@ -4,16 +4,16 @@
         .module("app")
         .controller("tabsController", ['authServices', '$scope', function(authServices, $scope) {
             var vm = this;
-            vm.showCorporate = false;
-            vm.showConsumer = false;
+            vm.showCorporate = true;
+            vm.showConsumer = true;
             console.log(authServices.getUserInfo().subType);
 
             if (authServices.getToken() && authServices.getUserInfo().subType.consumer === true) {
-                vm.showConsumer = true;
+                vm.showConsumer = false;
             }
 
             if (authServices.getToken() && authServices.getUserInfo().subType.corporate === true) {
-                vm.showCorporate = true;
+                vm.showCorporate = false;
             }
 
         }]);
