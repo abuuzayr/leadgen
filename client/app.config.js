02361f6f3c2d6380e7b4c1f2e1dde7dcae6471e3
Calvyn Tng <calvyn@groventure.com>
2016-09-20 18:09:19 +0800

    update config to laters url

--- client/app.config.js
+++ client/app.config.js
@@ -1,14 +1,14 @@
 angular.module('app')
     .constant('appConfig', {
         APP_VERSION: '0.1',
-        API_URL: '//10.4.1.213/api',
-        AUTH_URL: '//10.4.1.213/auth/user',
-        FP_URL: '//10.4.1.213/forgetpassword/user',
-        UM_URL: '//10.4.1.198/req/api/usermgmt',
+        API_URL: '//app.bulletleads.com/api',
+        // AUTH_URL: '//10.4.1.213/auth/user',
+        // FP_URL: '//10.4.1.213/forgetpassword/user',
+        // UM_URL: '//10.4.1.198/req/api/usermgmt',
         // UM_URL: '//10.4.1.213/usermgmt',
-        //        AUTH_URL: '//app.bulletsuite.com/api/auth/user',
-        //        FP_URL: '//app.bulletsuite.com/api/forgetpassword/user',
-        //        UM_URL: '//app.bulletsuite.com/api/usermgmt',
+               AUTH_URL: '//app.bulletsuite.com/api/auth/user',
+               FP_URL: '//app.bulletsuite.com/api/forgetpassword/user',
+               UM_URL: '//app.bulletsuite.com/api/usermgmt',
         MIN_PASSWORD_LENGTH: 8,
         MAX_PASSWORD_LENGTH: 24
     });
